a4638eeb3b92940c1d823c32eec97837bd92d14d
Ákos Balogh <akos.balogh01@gmail.com>
2020-07-09 13:39:38 +0200

    Project cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main(void) {
-    std::cout << "Hello world!" << std::endl;
+int main(int argc, char* argv[]) {
+    std::vector <std::string> songlist;
+    for (int i = 1; i < argc; i++) {
+        songlist.push_back(argv[i]);
+    }
+/*
+    SongPlayer sp(songlist);
+    Visualiser vs;
+
+    while (vs.isRunning()) {
+        vs.processEvents();
+        if (vs.isPaused) continue;
+
+        sp.play();
+    }
+
     return 0;
+    */
+
+    //window.setKeyRepeatEnabled(false);
 }
