554b4c02ae36d8211a5f0e64920894093b7b10fb
Ákos Balogh <akos.balogh01@gmail.com>
2020-08-20 13:36:13 +0200

    Fixed RGB data transmission.

--- src/vis/tx.cpp
+++ src/vis/tx.cpp
@@ -1,17 +1,18 @@
 #include "visualiser.hpp"
 
 void vs::visualiser::transmit() {
-    uint8_t buffer[vs::gfx::ledcount * 3] = {0};
-    DWORD written = 0;
-    unsigned size = vs::gfx::ledcount * 3;
+    unsigned bufsize = vs::gfx::ledcount * 3;
+    uint8_t  buffer[bufsize] = {0};
+    DWORD    written = 0;
+
     for (unsigned i = 0; i < vs::gfx::ledcount; ++i) {
         sf::Color c = playing->getBinColor(i);
-        float br = ((float) playing->getBrightness()/100);
-        float bh = ((float) playing->getBinHeight(i)/vs::gfx::layout::bheight)*100;
-        buffer[(i*3)] = br * bh * c.g;
+        unsigned br = playing->getBrightness(); //0-100
+        float bh = (float) playing->getBinHeight(i)/vs::gfx::layout::bheight; //0-1
+        buffer[(i*3)] = br * bh * c.g; //br * bh = percentage of selected bin color
         buffer[(i*3) + 1] = br * bh * c.r;
         buffer[(i*3) + 2] = br * bh * c.b;
     }
 
-    WriteFile(serial, buffer, size, &written, NULL);
+    WriteFile(serial, buffer, bufsize, &written, NULL);
 }
