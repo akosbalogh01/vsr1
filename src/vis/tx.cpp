ae7fcd908d31238c7a8308505b8a6e623ca12ecd
Ákos Balogh <akos.balogh01@gmail.com>
2020-08-21 11:19:09 +0200

    Updated LED TX.

--- src/vis/tx.cpp
+++ src/vis/tx.cpp
@@ -10,9 +10,9 @@ void vs::visualiser::transmit() {
         sf::Color c = playing->getBinColor(i);
         unsigned bha = playing->getBinHeight(i);
         float    bhr = (float) bha/vs::gfx::layout::bheight; //0-1
-        buffer[(i*3)]     = c.g + ((clrmax - c.g)*bhr);
-        buffer[(i*3) + 1] = c.r + ((clrmax - c.r)*bhr);
-        buffer[(i*3) + 2] = c.b + ((clrmax - c.b)*bhr);
+        buffer[(i*3)]     = c.g * bhr;// + ((clrmax - c.g)*bhr);
+        buffer[(i*3) + 1] = c.r * bhr;// + ((clrmax - c.r)*bhr);
+        buffer[(i*3) + 2] = c.b * bhr; //+ ((clrmax - c.b)*bhr);
     }
 
     WriteFile(serial, buffer, bufsize, &written, NULL);
