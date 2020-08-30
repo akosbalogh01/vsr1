883b12e1e794d3a7625a24767ed5add79ec2f1d9
Ákos Balogh <akos.balogh01@gmail.com>
2020-08-30 21:13:05 +0200

    Removed 0.0f frequency warping.

--- src/graphics/vis/fourier/update.cpp
+++ src/graphics/vis/fourier/update.cpp
@@ -5,7 +5,7 @@ void vs::vis::fourier::update() {
     std::vector<float> samples = playing->getSamples();
     vs::fft::func::window(samples);
     samples = vs::fft::func::warp(0.7f, samples);
-    samples = vs::fft::func::warp(0.0f, samples);
+    //samples = vs::fft::func::warp(0.0f, samples);
     bins = vs::fft::func::fft(samples);
 
     unsigned br = playing->getModBrightness();
