26290f7aae7d59204bef230e56b652fe1d366ac3
borszag <fractran@gmail.com>
2020-09-01 00:01:53 +0200

    Refine binheight calculation.

--- src/graphics/vis/fourier/binheight.cpp
+++ src/graphics/vis/fourier/binheight.cpp
@@ -2,7 +2,13 @@
 
 const unsigned vs::vis::fourier::binHeight(const unsigned bin) const {
     const unsigned div = 24; // log2(max value of value range) + log2(sample count), '15+9'
+    const double maxval =32768;
     const unsigned nbin = ((double) bin/vs::gfx::ledcount) * bins.size()/2;
-    double h = log2(sqrt(pow(bins[nbin].imag(), 2) + pow(bins[nbin].real(), 2)))/div;
-    return h * vs::gfx::layout::bheight;
+    double h = (log10(sqrt(pow(bins[nbin].imag(), 2) + pow(bins[nbin].real(), 2))/maxval)+2.5) * vs::gfx::layout::bheight/3;
+    if (h < 0.0) {
+        h = 0.0;
+    } else if (h > vs::gfx::layout::bheight) {
+        h = vs::gfx::layout::bheight;
+    }
+    return h;
 }
